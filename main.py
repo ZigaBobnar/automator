04e2ab728cb6d96b785732616773672f0527edea
Ziga Bobnar <ziga@bobnar.net>
2019-12-25 17:50:26 +0100

    Mouse listener example

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-import time
-from winapi.cursor import get_cursor_pos, set_cursor_pos
+from automator.mouse import Mouse
+# import time
+# from winapi.cursor import get_cursor_pos, set_cursor_pos
 
-delay = 0.01
-points = []
-for i in range(200):
-    points.append(get_cursor_pos())
-    time.sleep(delay)
+mouse = Mouse()
+mouse.listen()
 
-for i in points:
-    set_cursor_pos(i[0], i[1])
-    time.sleep(delay)
+# delay = 0.01
+# points = []
+# for i in range(200):
+#     points.append(get_cursor_pos())
+#     time.sleep(delay)
+
+# for i in points:
+#     set_cursor_pos(i[0], i[1])
+#     time.sleep(delay)
