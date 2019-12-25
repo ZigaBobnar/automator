bb44db758421fe1f5365c22c5f7af780af70148a
Ziga Bobnar <ziga@bobnar.net>
2019-12-25 17:51:44 +0100

    Remove old code

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
 from automator.mouse import Mouse
-# import time
-# from winapi.cursor import get_cursor_pos, set_cursor_pos
 
 mouse = Mouse()
 mouse.listen()
-
-# delay = 0.01
-# points = []
-# for i in range(200):
-#     points.append(get_cursor_pos())
-#     time.sleep(delay)
-
-# for i in points:
-#     set_cursor_pos(i[0], i[1])
-#     time.sleep(delay)
